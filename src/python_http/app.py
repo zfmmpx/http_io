60870c1a62446435929f6ba839ea5ff599b4d31d
Jabberwockeez <guoguozhiyu@gmail.com>
2019-05-04 18:54:57 +0800

    添加 python http 服务器

---
+++ src/python_http/app.py
@@ -0,0 +1,29 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+from pyramid.events import NewRequest
+
+
+def hello_world(request):
+    return Response('Hellow World')
+
+def add_cors_headers_response_callback(event):
+    def cors_headers(request, response):
+        response.headers.update({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Max-Age': '1728000',
+        })
+    event.request.add_response_callback(cors_headers)
+
+
+if __name__ == '__main__':
+    with Configurator() as config:
+        config.add_subscriber(add_cors_headers_response_callback, NewRequest)
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6543, app)
+    server.serve_forever()
