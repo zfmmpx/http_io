3f5f2b6b87703d18dbddb5960f585539b4349142
Jabberwockeez <guoguozhiyu@gmail.com>
2019-05-04 16:48:09 +0800

    '返回所有图片完整路径'

--- src/pic.py
+++ src/pic.py
@@ -5,7 +5,6 @@ path = os.path.dirname(os.path.dirname(__file__))
 pic_path = os.path.join(path, 'assets')
 
 file_list = os.listdir(pic_path)
+pic_list = []
 for file in file_list:
-    print(os.path.join(pic_path, file))
-
-# print(file_list)
+    pic_list.append(os.path.join(pic_path, file))
